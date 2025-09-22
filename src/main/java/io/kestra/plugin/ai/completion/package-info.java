702100b298194e9768b5d9e379a1c2080afcf5d5
Loïc Mathieu <loikeseke@gmail.com>
2025-09-22 16:44:57 +0200

    fix: add missing package-info.java
    Without them, the doc is not displayed on the website.
    
    Fixes https://github.com/kestra-io/docs/issues/3107

---
+++ src/main/java/io/kestra/plugin/ai/completion/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains AI completion tasks.",
+    categories = { PluginSubGroup.PluginCategory.AI }
+)
+package io.kestra.plugin.ai.completion;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
