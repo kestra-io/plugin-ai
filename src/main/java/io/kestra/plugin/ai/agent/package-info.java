09c0af960a6d41036c993124352a182e4bf29262
Loïc Mathieu <loikeseke@gmail.com>
2025-09-22 16:44:57 +0200

    fix: add missing package-info.java
    Without them, the doc is not displayed on the website.
    
    Fixes https://github.com/kestra-io/docs/issues/3107

---
+++ src/main/java/io/kestra/plugin/ai/agent/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains AI tasks.",
+    categories = { PluginSubGroup.PluginCategory.AI, PluginSubGroup.PluginCategory.DATABASE }
+)
+package io.kestra.plugin.ai.agent;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
