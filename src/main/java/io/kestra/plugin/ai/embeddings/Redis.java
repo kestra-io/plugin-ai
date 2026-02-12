abe8eed7560881529765ce2ea73ea51be51a24ce
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-12 16:56:16 +0100

    fix: compilation error

--- src/main/java/io/kestra/plugin/ai/embeddings/Redis.java
+++ src/main/java/io/kestra/plugin/ai/embeddings/Redis.java
@@ -84,7 +84,7 @@ public class Redis extends EmbeddingStoreProvider {
         );
 
         var store = RedisEmbeddingStore.builder()
-            .jedisPooled(jedis)
+            .unifiedJedis(jedis)
             .indexName(rIndexName)
             .dimension(dimension)
             .build();
