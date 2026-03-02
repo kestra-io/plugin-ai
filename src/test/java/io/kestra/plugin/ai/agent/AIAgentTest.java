ff16ab15bac23cbbd2bbb5a10e23305e3b05ba1c
François Delbrayelle <fdelbrayelle@kestra.io>
2026-03-02 11:15:08 +0100

    test(agent): align parentTraceId assertion with traceparent value

--- src/test/java/io/kestra/plugin/ai/agent/AIAgentTest.java
+++ src/test/java/io/kestra/plugin/ai/agent/AIAgentTest.java
@@ -151,7 +151,7 @@ class AIAgentTest {
     @Test
     void withLangfuseObservabilityIncludesParentTraceId() throws Exception {
         var traceParent = "00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01";
-        var parentTraceId = "4bf92f3577b34da6a3ce929d0e0e4736";
+        var parentTraceId = traceParent;
 
         WireMockServer wireMock = new WireMockServer(wireMockConfig().dynamicPort());
         try {
